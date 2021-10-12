3b23de5aaa860b58946a77ead0fa3ec47b7d1ee7
Tim Ingenbrand <68811601+timingenbrand@users.noreply.github.com>
2021-10-12 10:26:31 +0200

    Add files via upload

---
+++ GECC_performance/snm_simulation.cpp
@@ -0,0 +1,77 @@
+
+/*##############################################
+############## E. COLI SIMULATION ##############
+###############################################*/
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <time.h>
+#include <random>
+#include "snm_functions.h"
+#include "snm_variables.h"
+
+
+
+using namespace std;
+
+// Need to initalize arrays here because of "variable" length
+unsigned long long int x[9]; // species
+double amu[16]; // propensities
+
+
+
+int Simulation(string filename)
+{
+    SetUp(x);
+
+    double nextStoptime = dt;
+
+	// start of main loop
+    while (currentTime < endTime) {
+        CalculateAmu(amu, x); // calc propensities
+
+        r1 = (long double)(mt_rand()+1.)/(mt_rand.max()+1.);
+        tau = -log(r1) / amu[n_reactions - 1]; // generate new tau
+
+        while(tau>dt){ // does tau jump over the next save-time?
+          Movement(dt);
+          Output(&nextStoptime, x, filename);
+          nextStoptime += dt;
+          currentTime += dt;
+
+          if (currentTime > endTime) {
+			  //cout << "Break here" << endl;
+              breaksim = true;
+              break;
+            }
+          CalculateAmu(amu, x); // re-calc propensities after movement
+
+          r1 = (long double)(mt_rand()+1.)/(mt_rand.max()+1.);
+          tau = -log(r1) / amu[n_reactions - 1]; // re-generate tau
+        }
+        if (breaksim) {
+            break;
+        }
+        currentTime += tau;
+
+        if (runstate){ // do remaining movement
+            Movement(tau);
+        }
+
+        // determine which reaction is happening
+        r2 = amu[n_reactions - 1] *(long double)(mt_rand()+1.)/(mt_rand.max()+1.);
+        for (rIndex = 0; amu[rIndex] < r2; rIndex++);
+
+		// execute reaction
+        Reactions(rIndex, x);
+
+		// save data
+        if (currentTime > nextStoptime) {
+            Output(&nextStoptime, x, filename);
+            nextStoptime += dt;
+        }
+
+    }
+    return 0;
+}
